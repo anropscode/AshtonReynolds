71830c7d35468add840e109229e766bd2c8027dd
anropscode <ashrey12345678@gmail.com>
2025-07-30 11:15:27 +0100

    script.js

---
+++ task/script.js
@@ -0,0 +1,228 @@
+var status = [
+	    code: "200",
+	    name: "ok",
+	    description: "success",
+	    ReturnedIn: "370ms"
+]
+const geonames = [
+    {	
+		toponymName: "Abruzzo",
+        areaname: "Abruzzo",
+        lat: "42.25",
+        lng: "13.75",
+        geonameId: "3183560",
+        countryCode: "IT",
+        countryName: "Italy",
+        fcl: "A",
+        fcode: "ADM1"
+	},
+    {	
+		toponymName: "Regione Autonoma Valle d'Aosta",
+        areaname: "Aosta Valley",
+        lat: "45.75",
+        lng: "7.25",
+        geonameId: "3164857",
+        countryCode: "IT",
+        countryName: "Italy",
+        fcl: "A",
+        fcode: "ADM1"
+	},
+	{
+    	toponymName: "Puglia",
+    	areaname: "Apulia",
+    	lat: "41.00164",
+    	lng: "16.73378",
+    	geonameId: "3169778",
+    	countryCode: "IT",
+    	countryName: "Italy",
+   		fcl: "A",
+   		fcode: "ADM1"
+	},
+	{
+		toponymName: "Basilicata",
+    	areaname: "Basilicate",
+   		lat: "40.5041",
+    	lng: "16.11396",
+    	geonameId: "3182306",
+    	countryCode: "IT",
+    	countryName: "Italy",
+    	fcl: "A",
+    	fcode: "ADM1"
+	},
+	{
+		toponymName: "Calabria",
+		areaname: "Calabria",
+		lat: "39",
+		lng: "16.5",
+		geonameId: "2525468",
+		countryCode: "IT",
+		countryName: "Italy",
+		fcl: "A",
+		fcode: "ADM1"
+	},
+	{
+		toponymName: "Campania",
+		areaname: "Campania",
+		lat: "40.91056",
+		lng: "14.92053",
+		geonameId: "3181042",
+		countryCode: "IT",
+		countryName: "Italy",
+		fcl: "A",
+		fcode: "ADM1"
+	},
+	{
+		toponymName: "Emilia-Romagna",
+		areaname: "Emilia-Romagna",
+		lat: "44.5444",
+		lng: "10.98361",
+		geonameId: "3177401",
+		countryCode: "IT",
+		countryName: "Italy",
+		fcl: "A",
+		fcode: "ADM1"
+	},
+	{
+		toponymName: "Friuli Venezia Giulia",
+		areaname: "Friuli Venezia Giulia",
+		lat: "46",
+		lng: "13",
+		geonameId: "3176525",
+		countryCode: "IT",
+		countryName: "Italy",
+		fcl: "A",
+		fcode: "ADM1"
+	},
+	{	
+		toponymName: "Lazio",
+		areaname: "Lazio",
+		lat: "42.07762",
+		lng: "12.77878",
+		geonameId: "3174976",
+		countryCode: "IT",
+		countryName: "Italy",
+		fcl: "A",
+		fcode: "ADM1"
+	},
+	{	
+		toponymName: "Liguria",
+		areaname: "Liguria",
+		lat: "44.5",
+		lng: "8.83333",
+		geonameId: "3174725",
+		countryCode: "IT",
+		countryName: "Italy",
+		fcl: "A",
+		fcode: "ADM1"
+	},
+	{
+		toponymName: "Lombardia",
+		areaname: "Lombardy",
+		lat: "45.66667",
+		lng: "9.5",
+		geonameId: "3174618",
+		countryCode: "IT",
+		countryName: "Italy",
+		fcl: "A",
+		fcode: "ADM1"
+	},
+	{
+		toponymName: "Molise",
+		areaname: "Molise",
+		lat: "41.66667",
+		lng: "14.5",
+		geonameId: "3173222",
+		countryCode: "IT",
+		countryName: "Italy",
+		fcl: "A",
+		fcode: "ADM1"
+	},
+	{
+		toponymName: "Piemonte",
+		areaname: "Piedmont",
+		lat: "45",
+		lng: "8",
+		geonameId: "3170831",
+		countryCode: "IT",
+		countryName: "Italy",
+		fcl: "A",
+		fcode: "ADM1"
+	},
+	{
+		toponymName: "Sardegna",
+		areaname: "Sardinia",
+		lat: "40",
+		lng: "9",
+		geonameId: "2523228",
+		countryCode: "IT",
+		countryName: "Italy",
+		fcl: "A",
+		fcode: "ADM1"
+	},
+	{
+		toponymName: "Sicilia",
+		areaname: "Sicily",
+		lat: "37.75",
+		lng: "14.25",
+		geonameId: "2523119",
+		countryCode: "IT",
+		countryName: "Italy",
+		fcl: "A",
+		fcode: "ADM1"
+	},
+	{
+		toponymName: "Marche",
+		areaname: "The Marches",
+		lat: "43.5",
+		lng: "13.25",
+		geonameId: "3174004",
+		countryCode: "IT",
+		countryName: "Italy",
+		fcl: "A",
+		fcode: "ADM1"
+	},
+	{
+		toponymName: "Trentino-Alto Adige",
+		areaname: "Trentino-Alto Adige",
+		lat: "46.5",
+		lng: "11.33333"
+		geonameId: "3165244",
+		countryCode: "IT",
+		countryName: "Italy",
+		fcl: "A",
+		fcode: "ADM1"
+	},
+	{
+		toponymName: "Toscana",
+		areaname: "Tuscany",
+		lat: "43.41667",
+		lng: "11",
+		geonameId: "3165361",
+		countryCode: "IT",
+		countryName: "Italy",
+		fcl: "A",
+		fcode: "ADM1"
+	},
+	{
+		toponymName: "Umbria",
+		areaname: "Umbria",
+		lat: "43",
+		lng: "12.5",
+		geonameId: "3165048",
+		countryCode: "IT",
+		countryName: "Italy",
+		fcl: "A",
+		fcode: "ADM1"
+	},
+	{
+		toponymName: "Veneto",
+		areaname: "Veneto",
+		lat: "45.5",
+		lng: "11.75",
+		geonameId: "3164604",
+		countryCode: "IT",
+		countryName: "Italy",
+		fcl: "A",
+		fcode: "ADM1"
+	}
+];
