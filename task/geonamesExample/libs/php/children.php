95745b286bc9bf782031e73b9387676c1b0651e3
anropscode <ashrey12345678@gmail.com>
2025-08-05 14:53:33 +0100

    Update children.php

--- task/geonamesExample/libs/php/children.php
+++ task/geonamesExample/libs/php/children.php
@@ -4,19 +4,12 @@
 	
 	$executionStartTime = microtime(true);
 	
-	$url = "http://api.geonames.org/children?geonameId=3175395&username=demo"
-     . "&toponymName=" . urlencode($_REQUEST['toponymName']);
-     . "&areaname=" . urlencode($_REQUEST['areaname']);
-	 . "&lat=" . urlencode($_REQUEST['lat']);
-	 . "&lng=" . urlencode($_REQUEST['lng']);
-	 . "&geonameId=" . urlencode($_REQUEST['geonameId']);
-	 . "&CountryCode=" . urlencode($_REQUEST['CountryCode']);
-	 . "&CountryName=" . urlencode($_REQUEST['CountryName']);
-	 . "&fcl=" . urlencode($_REQUEST['fcl']);
-	 . "&fcode=" . urlencode($_REQUEST['fcode']);
+	
+	$geonameid = isset($_REQUEST['geonameid']) ? $_REQUEST['geonameid'] : '3175395';
+	$url = "http://api.geonames.org/children?geonameId=3175395" . urlencode($geonameid) . "&username=anropscode";
 
 	$ch = curl_init();
-	curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
 	curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch,CURLOPT_URL, $url);
 	
@@ -29,10 +22,11 @@
 	$output['status']['code'] = 200;
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
-	$output['status']['ReturnedIn'] = "intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $decode['geonameId'];
+	$output['status']['ReturnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+	$output['data'] = $decode['geonames'];
 	
 	header('Content-Type: application/json; charset-UTF-8');
 	
+	file_put_contents('debug.txt', print_r($decode, true));
 	echo json_encode($output);
 ?>
