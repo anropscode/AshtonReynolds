64aa0f012cd4582c7c38dc642bf03bba46ebfc06
anropscode <ashrey12345678@gmail.com>
2025-08-07 14:10:46 +0100

    Update script.js

--- task/geonamesExample/libs/js/script.js
+++ task/geonamesExample/libs/js/script.js
@@ -1,30 +1,18 @@
-$(document).ready(function() {
-$('#btnRun').click(function(){
-    $.ajax({
-        url: "http://api.geonames.org/children?geonameId=3175395&username=anropscode",
-        type: 'POST',
-        dataType: 'json',
-        data: {
-            geonameid: $('#geonameselect').val()
-        },
-        success: function(result) {
-            console.log(result);
-            if(result.status.name === "ok") {
-                $('#toponymname_result').html(result['data'][0]['toponymname']);
-                $('#name_result').html(result['data'][0]['name']);
-                $('#lat_result').html(result['data'][0]['lat']);
-                $('#lng_result').html(result['data'][0]['lng']);
-                $('#geonameid_result').html(result['data'][0]['geonameid']);
-                $('#countrycode_result').html(result['data'][0]['countrycode']);
-                $('#countryname_result').html(result['data'][0]['countryname']);
-                $('#fcl_result').html(result['data'][0]['fcl']);
-                $('#fcode').html(result['data'][0]['fcode']);
-            }
-        },
-        error: function(jqxhr, status, error) {
-            console.error("Error: " + error);
-            $('#error').html("Error fetching children data.");
-        }
-    });
-});
+$.ajax({
+    url: "libs/php/children.php",
+    type: 'POST',
+    dataType: 'json',
+    data: {
+      geonames: $('#geonames').val()
+    },
+    success: function(result) {
+
+      if (result.status == "ok") {
+        $('#lat').html(result['lat']);
+        $('#countryId').html(result['countryId']);
+      }
+    },
+    error: function(jqxhr, status, error) {
+      console.error("Error occurred:", status, error);
+    }
 });
