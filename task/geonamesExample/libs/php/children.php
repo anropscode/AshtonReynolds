e7f552bde6aa6fb435eb69579644bf0b08e19032
anropscode <ashrey12345678@gmail.com>
2025-08-07 14:12:28 +0100

    Update children.php

--- task/geonamesExample/libs/php/children.php
+++ task/geonamesExample/libs/php/children.php
@@ -1,32 +1,30 @@
 <?php
-	ini_set('display_errors', 'On');
-	error_reporting(E_ALL);
-	
-	$executionStartTime = microtime(true);
-	
-	
-	$geonameid = isset($_REQUEST['geonameid']) ? $_REQUEST['geonameid'] : '3175395';
-	$url = "http://api.geonames.org/children?geonameId=3175395" . urlencode($geonameid) . "&username=anropscode";
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
+    
+    $executionStartTime = microtime(true);
 
-	$ch = curl_init();
-	curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
-	curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch,CURLOPT_URL, $url);
-	
-	$result=curl_exec($ch);
-	
-	curl_close($ch);
-	
-	$decode = json_decode($result, true);
-	
-	$output['status']['code'] = 200;
-	$output['status']['name'] = "ok";
-	$output['status']['description'] = "success";
-	$output['status']['ReturnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $decode['geonames'];
-	
-	header('Content-Type: application/json; charset-UTF-8');
-	
-	file_put_contents('debug.txt', print_r($decode, true));
-	echo json_encode($output);
+    $url = "http://api.geonames.org/childrenJSON?geonameId=3175395&username=anropscode";
+    
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL,$url);
+
+    $result=curl_exec($ch);
+
+    curl_close($ch);
+
+    $decode = json_decode($result,true);    
+
+    $output['status']['code'] = "200";
+    $output['status']['name'] = "ok";
+    $output['status']['description'] = "success";
+    $output['status']['returnedIn'] = intval((microtime(true)-$executionStartTime) * 1000) . " ms";
+    $output['data'] = $decode;
+    
+    header('Content-Type: application/json; charset=UTF-8');
+
+    echo json_encode($output);
+
 ?>
